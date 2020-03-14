4ea5da5c04e5b23c309c830b985104d401f414b5
WarmBeer <mickvd99@gmail.com>
2020-03-14 12:21:27 +0100

    Setup.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Nav Bar</h2>
+        <nav>
+            <router-link to="/home">Home</router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Navigation"
+  }
+</script>
+
+<style scoped>
+
+</style>
