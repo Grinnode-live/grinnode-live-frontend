b95b475a8e7873f47fc4f70d5c7786e22a42f60e
MCM-Mike <m.c.mauer@gmail.com>
2020-03-21 23:55:08 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -154,7 +154,7 @@
                     You can get in contact with us <a href="https://github.com/MCM-Mike/grinnode.live/blob/master/contact.md">here</a>
 
                     <p style="text-decoration: underline;">Use Blocks not Bombs</p>
-                    <p><img width="300" height="121" src="images/blocks-not-bombs-878x1024.png" alt="Use Blocks not Bombs " /></p>
+                    <p><img width="200" height="190" src="images/blocks-not-bombs-878x1024.png" alt="Use Blocks not Bombs " /></p>
                 </div>
             </section>
         </main>
