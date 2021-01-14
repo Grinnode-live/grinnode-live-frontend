6d0e6e62c67d41a38b1b5107a0341f44799c5dce
stakervali <stakervali@yandex.com>
2021-01-14 12:23:02 +0300

    Revert "Contact page - added art from grinsun"
    This reverts commit faa601e78e4308c04e3a92391084dc274a8ccb73.

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -9,9 +9,7 @@
        <v-img contain height="500" :src="imageSrc">
 
        </v-img>
-
-      <v-caption v-if="!isGrinSun">art by <a href="https://github.com/lovelygrin">lovelygrin</a></v-caption>
-      <v-caption v-if="isGrinSun">art by <a href="https://github.com/lovelygrin">grinsun</a></v-caption>
+      <v-caption>Lovely art by <a href="https://github.com/lovelygrin">lovelygrin</a></v-caption>
     </v-col>
 
   </v-row>
@@ -52,38 +50,12 @@
           '/images/grinart/075_butwhatisagrin_500x.png'
       ];
 
-      let grinsunImageLinks =[
-          '/images/grinsun/0d9fb343612d6feb37d73b56a798599638ad7302.png',
-          '/images/grinsun/3dfc30823ab07076a0e7a51a9ec35a1c3b62d7eb.png',
-          '/images/grinsun/5ba158b37d94e88a62b90ac98e0d20f82c589b87.png',
-          '/images/grinsun/8e27feea6ea29290518db2316ce089b7a36feea7.png',
-          '/images/grinsun/170d978c6f6c9e67683387874c7e39fb2a113dcf.png',
-          '/images/grinsun/88646df7fda0af72ab86e12b1d586e1ea4b5c69f.png',
-          '/images/grinsun/123211cdb55904430c8723498e8e2139aeff47d0.png',
-          '/images/grinsun/a50b55b4b5e0514f26a19782978885c80936c57a.png',
-          '/images/grinsun/cfd0e1c09863781eb878d1dad554757320c03be3.png',
-          '/images/grinsun/e70e473f42b6c0272d6764c5962dcc9031b16eed.png',
-      ]
-
-       this.isGrinSun = Math.random() > 0.5;
-
-      if(this.isGrinSun){
-        let index = Math.round(Math.random()*grinsunImageLinks.length);
-        this.imageSrc = grinsunImageLinks[index];
-
-      }else{
-        let index = Math.round(Math.random()*imageLinks.length);
-        this.imageSrc = imageLinks[index];
-
-      }
-
-
-
+      let index = Math.round(Math.random()*imageLinks.length);
+      this.imageSrc = imageLinks[index];
     },
     data : function(){
       return {
-        imageSrc:"",
-        isGrinSun: false
+        imageSrc:""
       };
     }
   }
