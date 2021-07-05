55151f3854a10002d5a9de1a43715be796882e5c
stakervali <stakervali@yandex.com>
2021-07-05 14:00:34 +0300

    Added custom icon components

---
+++ src/components/icons/ChallengesIcon.vue
@@ -0,0 +1,20 @@
+<template>
+<!--  Watch icon from material icon set-->
+  <v-icon right>
+    {{svgPath}}
+  </v-icon>
+</template>
+
+<script>
+import {mdiWatch} from "@mdi/js";
+export default {
+  name: "ChallengesIcon",
+  data: ()=>({
+    svgPath:mdiWatch
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/icons/FaqIcon.vue
@@ -0,0 +1,20 @@
+<template>
+<!--  Question answer icon from material icon set-->
+  <v-icon right>
+    {{svgPath}}
+  </v-icon>
+
+</template>
+
+<script>
+export default {
+  name: "FaqIcon",
+  data: ()=>({
+    svgPath:"M21 6h-2v9H6v2c0 .55.45 1 1 1h11l4 4V7c0-.55-.45-1-1-1zm-4 6V3c0-.55-.45-1-1-1H3c-.55 0-1 .45-1 1v14l4-4h10c.55 0 1-.45 1-1z"
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
