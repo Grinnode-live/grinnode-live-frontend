96b549d55d73286af673550ed0e9ad4631bf22d3
stakervali <stakervali@yandex.com>
2022-06-09 19:41:42 +0300

    Added multi-axis, grin emission, legend and description

--- src/components/MarketCapChart.vue
+++ src/components/MarketCapChart.vue
@@ -1,13 +1,17 @@
 <template>
   <div style="width:100%">
-  <v-layout justify-center >
-    <v-radio-group row v-model="vs_currency" @change="updateChart"   >
-      <v-radio label="Bitcoin" value="btc"></v-radio>
-      <v-radio label="Monero" value="monero"></v-radio>
-    </v-radio-group>
-  </v-layout>
-
-    <canvas id="marketcapChart" height="200" ></canvas>
+    <p>
+      Assumed Grin price is calculated by answering the question what if the market cap of grin is equalivalent to
+      market cap of another crypto
+      (such as Bitcoin and Monero) considering the amount of emission.
+    </p>
+    <v-layout justify-center>
+      <v-radio-group row v-model="vs_currency" @change="updateChart">
+        <v-radio label="Bitcoin" value="btc"></v-radio>
+        <v-radio label="Monero" value="monero"></v-radio>
+      </v-radio-group>
+    </v-layout>
+    <canvas id="marketcapChart" height="200"></canvas>
   </div>
 
 </template>
@@ -19,31 +23,24 @@ import {
   ArcElement,
   BarController,
   BarElement,
-  //BubbleController,
-  //CategoryScale,
   Chart,
   Decimation,
-  //DoughnutController,
   Filler,
   Legend,
   LinearScale,
   LineController,
   LineElement,
-  // LogarithmicScale,
-  // PieController,
   PointElement,
-  //PolarAreaController,
-  // RadarController,
-  //RadialLinearScale,
-  ScatterController, SubTitle,
+  ScatterController,
+  SubTitle,
   TimeScale,
-  // TimeSeriesScale,
-  Title, Tooltip
+  Title,
+  Tooltip
 } from "chart.js";
 
 
-Chart.register( ArcElement, LineElement,
-    BarElement,    PointElement,    BarController,
+Chart.register(ArcElement, LineElement,
+    BarElement, PointElement, BarController,
     LineController,
     ScatterController,
     LinearScale,
@@ -59,145 +56,183 @@ Chart.register( ArcElement, LineElement,
 
 export default {
   name: "MarketCapChart",
-  data :  function(){
+  data: function () {
     return {
-      grin_usd : 0.114,
-      grin_cap : [],
-      btc_cap :   [],
-      monero_cap : [],
+      grin_usd: 0.114,
+      grin_cap: [],
+      btc_cap: [],
+      monero_cap: [],
       vs_currency: 'btc',
-      chart : '',
+      grinGenesisDate: new Date(2019, 0, 15),
+      chart: '',
+
     }
   },
-  methods:{
-    calculate_chart_data: function(grin_cap_series,vscap,grin_usd_price){
+  methods: {
+    calculate_chart_data: function (grin_cap_series, vscap, grin_usd_price) {
       let chartData = []
-      for (let i = 0;i<grin_cap_series.length;i++){
+      for (let i = 0; i < grin_cap_series.length; i++) {
         const date_long = grin_cap_series[i][0];
         // find correspending vs_currency cap
-        let correspending_cap = vscap.filter(c=>c[0] === date_long);
+        let correspending_cap = vscap.filter(c => c[0] === date_long);
 
-        if (correspending_cap.length>0){
-            let t = new Date(date_long);
-            t = new Date(t.getTime() + t.getTimezoneOffset()*60*1000);
-            let ratio = correspending_cap[0][1] / grin_cap_series[i][1]
-            chartData.push({
-              date : t,
-              grin_cap_price : ratio * grin_usd_price
-            })
+        if (correspending_cap.length > 0) {
+          let t = new Date(date_long);
+          t = new Date(t.getTime() + t.getTimezoneOffset() * 60 * 1000);
+          let ratio = correspending_cap[0][1] / grin_cap_series[i][1]
+          chartData.push({
+            date: t,
+            grin_cap_price: ratio * grin_usd_price
+          })
         }
       }
-
+      console.log("chartData: ", chartData);
       return chartData;
     },
-    updateChart : function(){
+    updateChart: function () {
+      console.log('updating chart...');
       this.chart.data = {
-        datasets:[{
-          label: " ",
+        datasets: [{
+          label: "Grin Assumed Price ($)",
+          type: 'line',
           data: this.chartData,
           parsing: {
             xAxisKey: 'date',
             yAxisKey: 'grin_cap_price'
+          },
+          borderColor: 'green',
+          backgroundColor: 'green',
+          pointRadius: 0,
+          borderWidth: 2
+
+        },
+          {
+            label: "emission",
+            type: 'line',
+            data: this.grinEmissionData,
+            parsing: {
+              xAxisKey: 'date',
+              yAxisKey: 'emission'
+            },
+            yAxisID: 'y1',
+            borderColor: '#fef102',
+            backgroundColor: '#fef102',
+            borderWidth: 2,
+            pointStyle: 'circle',
+            pointRadius: 0,
+            tooltip: {
+              enabled: false
+            }
           }
-          , pointBorderColor: "hsl(57,99.2%,30.2%)"
-          , pointBackgroundColor: 'hsl(57,99.2%,50.2%)'
-          , backgroundColor: 'hsl(57,99.2%,50.2%)'
-        }]
+        ]
       };
-      console.log(this.chartData);
+
       this.chart.update();
     }
   },
-  computed : {
-    chartData : function(){
-        let vs_cap = []
-        if (this.vs_currency === 'btc'){
-          vs_cap = this.btc_cap;
-        } else if (this.vs_currency === 'monero'){
-          vs_cap = this.monero_cap;
-        }
-        return this.calculate_chart_data(this.grin_cap, vs_cap,this.grin_usd);
+  computed: {
+    chartData: function () {
+      let vs_cap = []
+      if (this.vs_currency === 'btc') {
+        vs_cap = this.btc_cap;
+      } else if (this.vs_currency === 'monero') {
+        vs_cap = this.monero_cap;
+      }
+      return this.calculate_chart_data(this.grin_cap, vs_cap, this.grin_usd);
+    },
+    daysSinceGrinGenesis: function () {
+      const now = new Date();
+      return Math.floor((now.getTime() - this.grinGenesisDate.getTime()) / 1000 / 3600 / 24);
+    },
+    grinEmissionData: function () {
+      const now = new Date();
+      const currentEmission = Math.round((now.getTime() - this.grinGenesisDate.getTime()) / 1000);
+      console.log('grinEmissionData:', [
+        {date: this.grinGenesisDate, emission: 0},
+        {date: now, emission: currentEmission}
+      ]);
+      return [
+        {date: this.grinGenesisDate, emission: 0},
+        {date: now, emission: currentEmission}
+      ];
     }
   },
-  mounted(){
+  mounted() {
 
-    const monero_cap_profile_url = "https://api.coingecko.com/api/v3/coins/monero/market_chart?vs_currency=usd&days=30&interval=daily"
-    const bitcoin_cap_profile_url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30&interval=daily"
-    const grin_cap_profile_url = "https://api.coingecko.com/api/v3/coins/grin/market_chart?vs_currency=usd&days=30&interval=daily"
+    const monero_cap_profile_url = `https://api.coingecko.com/api/v3/coins/monero/market_chart?vs_currency=usd&days=${this.daysSinceGrinGenesis}&interval=daily`;
+    const bitcoin_cap_profile_url = `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=${this.daysSinceGrinGenesis}&interval=daily`;
+    const grin_cap_profile_url = `https://api.coingecko.com/api/v3/coins/grin/market_chart?vs_currency=usd&days=${this.daysSinceGrinGenesis}&interval=daily`;
 
     fetch(monero_cap_profile_url)
-        .then(resp=>resp.json())
-        .then(data=>{
+        .then(resp => resp.json())
+        .then(data => {
           this.monero_cap = data['market_caps'];
 
           fetch(grin_cap_profile_url)
-              .then(resp=>resp.json())
-              .then(data=>{
+              .then(resp => resp.json())
+              .then(data => {
                 this.grin_cap = data['market_caps'];
-                let lastPriceIndex = data['prices'].length-1;
+                let lastPriceIndex = data['prices'].length - 1;
                 this.grin_usd = data['prices'][lastPriceIndex][1];
 
                 fetch(bitcoin_cap_profile_url)
-                    .then(resp=>resp.json())
-                    .then(data=>{
+                    .then(resp => resp.json())
+                    .then(data => {
                       this.btc_cap = data['market_caps'];
                       this.updateChart();
                     })
-
               })
-
         })
-
-
     let chartCtx = document.getElementById('marketcapChart');
-
-
-
-    let time_unit = 'day';
-    let chartInput ={
-      type : 'bar',
-      data : {
-        datasets:[{
-          label: " ",
-          data: this.chartData,
-          parsing: {
-            xAxisKey: 'date',
-            yAxisKey: 'grin_cap'
-          }
-          , pointBorderColor: "hsl(57,99.2%,30.2%)"
-          , pointBackgroundColor: 'hsl(57,99.2%,50.2%)'
-          , backgroundColor: 'hsl(57,99.2%,50.2%)'
-        }]
+    let time_unit = 'year';
+    let chartInput = {
+      type: 'line',
+      data: {
+        datasets: []
       },
-      options : {
+      options: {
         plugins: {
           title: {display: false, text: "custom"},
           subtitle: {display: false},
-          legend: {display: false}
+          legend: {
+            display: true,
+            position: 'bottom',
+          }
         },
         scales: {
           x: {
             type: 'time',
             title: {
-              color: 'hsl(57,99.2%,10.2%)',
+              color: 'gray',
               display: true,
               text: ''
             },
             grid: {
-              tickColor: 'hsl(57,99.2%,30.2%)',
-              borderColor: 'hsl(57,99.2%,30.2%)'
+              tickColor: 'gray',
+              borderColor: 'gray'
             },
-            time: {unit: time_unit  },
+            time: {unit: time_unit},
 
           },
           y: {
             title: {
               color: 'hsl(57,99.2%,10.2%)',
               display: true,
-              text: 'Grin Hypothetical Price ($)'
+              text: 'Grin Assumed Price ($)'
             },
             grid: {
-              borderColor: 'hsl(57,99.2%,30.2%)'
+              borderColor: 'gray'
+            }
+          },
+          y1: {
+            type: 'linear',
+            title: {
+              display: true,
+              text: 'Grin emission',
+            },
+            position: 'right',
+            grid: {
+              drawOnChartArea: false,
             }
           }
         },
@@ -208,7 +243,7 @@ export default {
       }
     }
 
-    this.chart = new Chart(chartCtx,chartInput)
+    this.chart = new Chart(chartCtx, chartInput)
     this.updateChart();
   }
 
