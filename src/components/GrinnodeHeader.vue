e4f84f815422b220a97a6b725f255afc71e477d4
stakervali <stakervali@yandex.com>
2022-06-10 16:32:48 +0300

    update text to 6/2022

--- src/components/GrinnodeHeader.vue
+++ src/components/GrinnodeHeader.vue
@@ -2,7 +2,7 @@
   <nav>
     <v-system-bar absolute app dark class="grey darken-2">
       <span class="hidden-sm-and-down">
-        [02/2022] Grinnode.live is updated!
+        [06/2022] Grinnode.live is updated!
       </span>
       <v-spacer></v-spacer>
 
